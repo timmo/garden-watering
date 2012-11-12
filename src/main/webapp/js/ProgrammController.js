21385f62bc7d71ed8a154287baea580bcc811451
nitram509 <maki@bitkings.de>
2012-11-12 19:55:45 +0100

    hacking in train, erster Ansatz mit AngularJS

---
+++ src/main/webapp/js/ProgrammController.js
@@ -0,0 +1,11 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: maki
+ * Date: 12.11.12
+ * Time: 18:43
+ * To change this template use File | Settings | File Templates.
+ */
+
+function ProgrammController($scope) {
+    $scope.status = sampleData;
+}
---
+++ src/main/webapp/json/sampledata.js
@@ -0,0 +1,19 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: maki
+ * Date: 12.11.12
+ * Time: 18:32
+ * To change this template use File | Settings | File Templates.
+ */
+var sampleData = {
+    time : "21:10",
+    program : {
+        startTime : "21:00",
+            status : "running",
+            outlets : [
+            { id : "1", name : "Outlet 1", duration : 20, remaining : 10, status : "open" },
+            { id : "2", name : "Outlet 2", duration : 20, remaining : 20, status : "closed" },
+            { id : "3", name : "Outlet 3", duration : 20, remaining : 20, status : "closed" }
+        ]
+    }
+}
