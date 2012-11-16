27b11d067d2a423da0e6ab931f2a62fe085ad96c
Timmo Freudl-Gierke <timmo.gierke@gmail.com>
2012-11-16 12:54:11 +0100

    filter round two

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -13,4 +13,11 @@ angular.module('filters', [])
                 }
                 return "n.n.";
               };
+            })
+    .filter('firstUpTailLower', function () {
+              return function (status) {
+                status = status.toLowerCase();
+                status = status[0].toUpperCase() + status.length > 1 ? status.substr(1) : "";
+                return status;
+              };
             });
