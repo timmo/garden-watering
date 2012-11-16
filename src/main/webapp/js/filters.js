79b02c8cd748e08c9d15477cbbfde8b42eb3427a
Timmo Freudl-Gierke <timmo.gierke@gmail.com>
2012-11-16 15:16:35 +0100

    removed filter ProgramStatus

--- src/main/webapp/js/filters.js
+++ src/main/webapp/js/filters.js
@@ -1,23 +1,8 @@
 angular.module('filters', [])
-    .filter('ProgramStatus', function () {
-              return function (status) {
-                if (typeof status == "undefined") {
-                  return "n.n";
-                }
-                status = status.toUpperCase();
-                if (status == "RUNNING") {
-                  return "Running";
-                }
-                if (status == "IDLE") {
-                  return "Idle";
-                }
-                return "n.n.";
-              };
-            })
     .filter('firstUpTailLower', function () {
               return function (status) {
                 status = status.toLowerCase();
-                status = status[0].toUpperCase() + status.length > 1 ? status.substr(1) : "";
+                status = status[0].toUpperCase() + (status.length > 1 ? status.substr(1) : "");
                 return status;
               };
             });
